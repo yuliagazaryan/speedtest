8d33346aa0bb3434cc7c45e025516dd4597b87c0
BC-macair010 <bc-macair010@BC-macair010s-MacBook-Air.local>
2021-11-15 13:45:34 +0200

    timer

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import Circle from './Circle';
 import { circles } from './circles';
 
+const getRndInteger = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  };
+
 class App extends Component {
     state = {  
         score: 0,
+        current: 0,
      };
+
+     timer = undefined;
+     pace = 1500;
 
      clickHandler = () => {
          this.setState({
@@ -13,6 +21,31 @@ class App extends Component {
          });
      }
 
+     nextCircle = () => {
+        let nextActive;
+
+        do {
+            nextActive = getRndInteger(1, 4);
+        } while (nextActive === this.state.current);
+
+        this.setState({
+            current: nextActive,
+        });
+
+        this.pace += 0.95;
+        this.timer = setTimeout(this.nextCircle, this.pace);
+
+        console.log("active circle is", this.state.current);
+     };
+
+     startHandler = () => {
+        this.nextCircle();
+     };
+
+     stopHandler = () => {
+        clearTimeout(this.timer);
+    }
+    
     render() {
         return (
             <div>
@@ -28,8 +61,8 @@ class App extends Component {
                 ))}
                 </div>
             <div id="buttons">
-                <button>Start</button>
-                <button>Stop</button>
+                <button onClick={this.startHandler}>Start</button>
+                <button onClick={this.stopHandler}>Stop</button>
             </div>
             </div>
         );
