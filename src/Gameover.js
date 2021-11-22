bc6967449fabc6d20bba6109e20dc6ba409da8cf
BC-macair010 <bc-macair010@BC-macair010s-MacBook-Air.local>
2021-11-22 14:38:42 +0200

    button fix

--- src/Gameover.js
+++ src/Gameover.js
@@ -4,9 +4,9 @@ const Gameover = (props) => {
     return (
         <div id="container">
         <div id="gameover">
+        <button onClick={props.close}>X</button>
             <h2>Game over</h2>
             <p>Your score was: {props.score}</p>
-            <button onClick={props.close}>X</button>
         </div>
         </div>
     );
